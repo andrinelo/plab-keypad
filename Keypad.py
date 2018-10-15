751bc06cc4c5c591ab32598868f3ee91fdb8a1f3
toragunstad <toragunstad@gmail.com>
2018-10-15 14:07:34 +0200

    keypad update

--- Keypad.py
+++ Keypad.py
@@ -1,21 +1,64 @@
+import RPi.GPIO as GPIO
+import time
+
 #Keypad - interface to the physical keypad
+class Keypad():
 
-def setup():
-    #Set the proper mode via:
-    # GPIO.setmode(GPIO.BCM).
-    # Also, use GPIO functions to set the row pins
-    # as outputs and the column pins as inputs.
+    #hvordan connecte R og C
+    #hva skal vare i listen med rowpins og columnpins?
 
-    return 0
+    def setup(self):
+        rowpins = [] #list of rowpins?
+        columnpins= [] #list of columnpins
 
-def do_polling():
-    #Use nested loops (discussed above) to determine
-    # the key currently being pressed on the keypad.
+        #Set the proper mode via:
+        GPIO.setmode(GPIO.BCM)
 
-    return 0
+        #sets the row pins as outputs
+        for rp in rowpins:
+            GPIO.setup(rp,GPIO.OUT)
 
-def get_next_signal():
-    #This is the main interface between the agent and the keypad.
-    #It should initiate repeated calls to do polling until a key press is detected.
-    return 0
+        #sets the column pins as inputs
+        for cp in columnpins:
+            GPIO.setup(cp, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+        return 0
+
+    def do_polling(self):
+        #Use nested loops (discussed above) to determine
+        #the key currently being pressed on the keypad.
+
+        location = (0,0) #sets location being pushed to (0,0)
+
+        for rp in range(0,4):
+            #setting the RP to HIGH one at a time
+            GPIO.output(rp,GPIO.HIGH)
+
+            #looping through columns
+            for cp in range(0,3):
+                #check to see if column is HIGH
+                #20 times with delay to make sure its correct
+
+                count = 0
+                for i in range(0,20):
+                    if(GPIO.input(cp) == GPIO.HIGH):
+                        count += 1
+                    time.sleep(20)
+
+                if count == 20:
+                    # the key being pressed
+                    location = (rp, cp)
+
+            GPIO.output(rp, GPIO.low)
+
+
+
+
+
+        return 0
+
+    def get_next_signal(self):
+        #This is the main interface between the agent and the keypad.
+        #It should initiate repeated calls to do polling until a key press is detected.
+        return 0
 
