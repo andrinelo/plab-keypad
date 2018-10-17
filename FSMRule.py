83e75330dd2a3dde4d4edfb2c2f167740874c3fb
Helle Børset Eidissen <helleeidissen@gmail.com>
2018-10-17 15:30:55 +0200

    troor FSM er ok ish nå

--- FSMRule.py
+++ FSMRule.py
@@ -1,3 +1,6 @@
+from FSM import FSM
+from inspect import isfunction
+
 class FSMRule:
 
     def __init__(self, s1, s2, trigger, action):
@@ -5,3 +8,11 @@ class FSMRule:
         self.s2 = s2
         self.trigger = trigger
         self.action = action
+
+    def trigger_is_true(self, sign):
+        if isfunction(self.trigger):
+            return self.trigger(sign)
+        if FSM.signal_is_digit(self.trigger):
+            return sign == self.trigger
+        return False
+
