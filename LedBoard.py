d5b60c2a518a1f00d2aaff1574ae6bdd52f6da8d
Helle Børset Eidissen <helleeidissen@gmail.com>
2018-10-24 11:08:12 +0200

    Ledboard fixed charlieplex

--- LedBoard.py
+++ LedBoard.py
@@ -2,57 +2,51 @@
 import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 from time import sleep     # Import the sleep function from the time module
 
+
+
 class LedBoard:
+    pins = [13, 19, 26]
+
+    pin_led_states = [
+        [1, 0, - 1],
+        [0, 1, - 1],
+        [-1, 1, 0],
+        [-1, 0, 1],
+        [1, -1, 0],
+        [0, -1, 1]
+    ]
 
 
     def setup(self):
+
         #Set the proper mode via: GPIO.setmode(GPIO.BCM).
         GPIO.setmode(GPIO.BCM)  #Use physical pin numbering
 
-        GPIO.setup(1, GPIO.OUT)
-        GPIO.output(1, GPIO.LOW)
-        GPIO.setup(2, GPIO.OUT)
-        GPIO.output(2, GPIO.LOW)
-        GPIO.setup(3, GPIO.OUT)
-        GPIO.output(3, GPIO.LOW)
-        GPIO.setup(4, GPIO.OUT)
-        GPIO.output(4, GPIO.LOW)
-        GPIO.setup(5, GPIO.OUT)
-        GPIO.output(5, GPIO.LOW)
-        GPIO.setup(6, GPIO.OUT)
-        GPIO.output(6, GPIO.LOW)
-
-        # Set pin 1 - 6 to be an output pin and set initial value to low (off)
+    @staticmethod
+    def set_pin(pin_index, pin_state):
+        if pin_state == -1:
+            GPIO.setup(LedBoard.pins[pin_index], GPIO.IN)
+        else:
+            GPIO.setup(LedBoard.pins[pin_index], GPIO.OUT)
+            GPIO.output(LedBoard.pins[pin_index], pin_state)
 
 
-    def light_led(self, lid, ldur):
-        print("Ledboard lighhling light {} for {} seconds", lid, ldur)
-        GPIO.output(lid, GPIO.HIGH)
-        sleep(ldur)
-        GPIO.output(lid, GPIO.LOW)
-        #Turn on one of the 6 LEDs by making the appropriate combination of input and output
-        #declarations, and then making the appropriate HIGH /LOW settings on the output pins.
+    def light_led(self, led_number):
+        for pin_index, pin_state in enumerate(LedBoard.pin_led_states[led_number]):
+            LedBoard.set_pin(pin_index, pin_state)
 
     def flash_all_leds(self):
         print("Flashing all leds")
         #Flash all 6 LEDs on and off for k seconds, where k is an argument of the method.
-        GPIO.output(1, GPIO.HIGH)  # Turn on led 1
-        GPIO.output(2, GPIO.HIGH)  # Turn on led 2
-        GPIO.output(3, GPIO.HIGH)  # Turn on led 3
-        GPIO.output(4, GPIO.HIGH)  # Turn on led 4
-        GPIO.output(5, GPIO.HIGH)  # Turn on led 5
-        GPIO.output(6, GPIO.HIGH)  # Turn on led 6
+        for x in range (0,6):
+            self.light_led(x)
 
         sleep(3)  # Light for 3 seconds
 
-        GPIO.output(1, GPIO.LOW) # turn off led 1
-        GPIO.output(2, GPIO.LOW)  # turn off led 2
-        GPIO.output(3, GPIO.LOW)  # turn off led 3
-        GPIO.output(4, GPIO.LOW)  # turn off led 4
-        GPIO.output(5, GPIO.LOW)  # turn off led 5
-        GPIO.output(6, GPIO.LOW)  # turn off led 6
+        for y in LedBoard.pins:
+            GPIO.output(y, GPIO.LOW) # turn off led
 
-        print("flash all leds for k seconds complete")
+
 
     def twinkle_all_leds(self):
         print("twinkling all leds")
@@ -60,41 +54,38 @@ class LedBoard:
 
         time = (0.5)
 
-        GPIO.output(1, GPIO.HIGH)  # Turn on led 1
+        self.light_led(0)
         sleep(time)  # light for k/6 seconds
-        GPIO.output(1, GPIO.LOW)  # Turn off led 1
 
-        GPIO.output(2, GPIO.HIGH)  # Turn on led 2
+        self.light_led(1)
         sleep(time)  # light for k/6 seconds
-        GPIO.output(2, GPIO.LOW)  # Turn off led 2
 
-        GPIO.output(3, GPIO.HIGH)  # Turn on led 3
+        self.light_led(2)
         sleep(time)  # light for k/6 seconds
-        GPIO.output(3, GPIO.LOW)  # Turn off led 3
 
-        GPIO.output(4, GPIO.HIGH)  # Turn on led 4
+        self.light_led(3)
         sleep(time)  # light for k/6 seconds
-        GPIO.output(4, GPIO.LOW)  # Turn off led 4
 
-        GPIO.output(5, GPIO.HIGH)  # Turn on led 5
+        self.light_led(4)
         sleep(time)  # light for k/6 seconds
-        GPIO.output(5, GPIO.LOW)  # Turn off led 5
 
-        GPIO.output(6, GPIO.HIGH)  # Turn on led 6
+        self.light_led(5)
         sleep(time)  # light for k/6 seconds
-        GPIO.output(6, GPIO.LOW)  # Turn off led 6
+
+        for y in LedBoard.pins:
+            GPIO.output(y, GPIO.LOW) # turn off led
 
 
 
     def power_up(self):
         print("powering up in led board")
-        self.light_led(1, 3)
+        self.light_led(0)
 
     def power_down(self):
         print ("powering down in ledboard")
-        self.light_led(2, 1)
-        self.light_led(4, 1)
-        self.light_led(6, 1)
+        self.light_led(1)
+        self.light_led(3)
+        self.light_led(5)
 
     #in addition:
 # methods for the lighting patterns associated with powering up (and down) the system.
